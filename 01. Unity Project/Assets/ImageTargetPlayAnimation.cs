0679249c18afd2e45a97cfa120397f17519791ed
samerboules <samerboules@gmail.com>
2019-07-17 14:16:31 +0200

    Jumping atkins

---
+++ 01. Unity Project/Assets/ImageTargetPlayAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAnimation : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            gameObject.GetComponent<Animator>().Play("start");
+        }
+        else
+        {
+            // Stop audio when target is lost
+            //audio.Stop();
+        }
+    }
+}
