332ea68ce21d41090d040d2af514bc862a69494c
samerboules <samerboules@gmail.com>
2019-07-25 11:58:40 +0200

    I Couldn't use the hole picture as a marker. For some unknown reason the hole picture is much more difficult for the camera to detect. Marker has to be of square dimensions and without transparent layer. (Only .jpg files are accepted). For the time being, I used the original app icon but if you want to send me another pic, that could be done also. New jump animation integrated. New idle animation integrated. Atkins voice added and facial expressions are created. Atkins logo dimensions fixed. Button added (No functionality yet obviously)

--- 01. Unity Project/Assets/StartJumpAudio.cs
+++ 01. Unity Project/Assets/StartJumpAudio.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetScaleBeforeJump : StateMachineBehaviour
+public class StartJumpAudio : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        GameObject JumpingAtkins = GameObject.Find("JumpingAtkins");
+        AudioSource JumpAudio = JumpingAtkins.GetComponent<AudioSource>();
+        JumpAudio.Play(0);    
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,12 +19,10 @@ public class SetScaleBeforeJump : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        //Set Scale and play jump sound
-        GameObject JumpingAtkins = GameObject.Find("JumpingAtkins");
-        JumpingAtkins.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
